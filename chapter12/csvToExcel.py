c86b8e55ea10158c16bd1abb5ac49bd814d8a833
Laurence-mvt <laurencefinch@brandeis.edu>
2021-02-18 23:01:30 -0500

    added date formatting to excel filename

--- chapter12/csvToExcel.py
+++ chapter12/csvToExcel.py
@@ -1,9 +1,12 @@
 # csvToExcel - adds alumni csv files for different locations to excel file as different sheets (for educational purposes only)
 
 import csv
-import openpyxl
+import openpyxl, logging, datetime
 from pathlib import Path
 from openpyxl.utils import get_column_letter
+
+logging.basicConfig(level=logging.DEBUG, format=' \n%(asctime)s -  %(levelname)s -  %(message)s\n')
+logging.disable(logging.CRITICAL)
 
 # get csv files paths from directory
 p = Path('/Users/laurencefinch/Desktop/AutomateBoringStuff/alumScrapeResults')
@@ -15,6 +18,8 @@ sheet = wb.active
 
 # loop over csv files
 for csvFilePath in csvFilePaths:
+
+    logging.info(f'Now working on csvFile {csvFilePath}')
     
     # open csv file and get location from name and alums in csvDictReader
     csvFile = open(csvFilePath)
@@ -37,11 +42,27 @@ for csvFilePath in csvFilePaths:
     # add alumni data
     # loop over all alums
     rowNum = 2  # first alum's row, after headers
+    for i, alum in enumerate(alumList[len(alumList) - 2:]):
+        logging.debug(f'{alum}')
+        # loop over fields for that alum's row
+        for index, header in enumerate(headers):
+            sheet[f'{get_column_letter(index+1)}{rowNum}'] = alumList[len(alumList) - 2 + i][header]
+        rowNum += 1
+
+        rowNum = 2  # first alum's row, after headers
     for i, alum in enumerate(alumList[1:]):
+        logging.debug(f'{alum}')
         # loop over fields for that alum's row
         for index, header in enumerate(headers):
             sheet[f'{get_column_letter(index+1)}{rowNum}'] = alumList[i][header]
         rowNum += 1
+    # add last alum
+    for index, header in enumerate(headers):
+        sheet[f'{get_column_letter(index+1)}{rowNum}'] = alumList[-1][header]
     
-    # save workbook
-    wb.save('/Users/laurencefinch/Desktop/AutomateBoringStuff/alumList3.xlsx')
+# save workbook
+# get datetime
+updateTime = datetime.datetime.now()
+# format to ddMonyyyy
+updateTime = updateTime.strftime('%d%b%Y')
+wb.save(f'/Users/laurencefinch/Desktop/AutomateBoringStuff/alumScrapeResults/alumList4_{updateTime}.xlsx')
